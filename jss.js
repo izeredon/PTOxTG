129cbbb941146ceeafcba35ed6f93485fc174707
izeredon <didiodon2@gmail.com>
2023-05-14 16:44:50 +0200

    change

--- jss.js
+++ jss.js
@@ -1,11 +1,18 @@
-function dnaToRna(dnaString) {
-    let rnaString = '';
-    for (let i = 0; i < dnaString.length; i++) {
-      if (dnaString[i] === 'T') {
-        rnaString += 'U';
-      } else {
-        rnaString += dnaString[i];
-      }
-    }
-    return rnaString
-  }
+const str1 = "1010";
+const str2 = "1011";
+const addBinary = (str1, str2) => {
+   let carry = 0;
+   const res = [];
+   let l1 = str1.length, l2 = str2.length;
+   for (let i = l1 - 1, j = l2 - 1; 0 <= i || 0 <= j; --i, --j) {
+      let a = 0 <= i ? Number(str1[i]) : 0,
+      b = 0 <= j ? Number(str2[j]) : 0;
+      res.push((a + b + carry) % 2);
+      carry = 1 < a + b + carry;
+   };
+   if (carry){
+      res.push(1);
+   }
+   return res.reverse().join('');
+};
+console.log(addBinary(str1, str2));
